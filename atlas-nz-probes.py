69f05179cdf16709c62e75c623f46c2041d90004
Sebastian Castro <sebastian@nzrs.net.nz>
2014-12-15 09:31:36 +1300

    Save the list of ASes hosting a probe to fetch the names. Count the number of probes on each ASes to determine coverage

--- atlas-nz-probes.py
+++ atlas-nz-probes.py
@@ -3,6 +3,8 @@
 import urllib2
 import json
 import sys
+from collections import Counter
+import pandas as pd
 
 base_url = "https://atlas.ripe.net/"
 url = base_url + "/api/v1/probe/?country_code=NZ&format=json&limit=50"
@@ -11,6 +13,7 @@ request = urllib2.Request(url)
 request.add_header("Content-Type", "application/json")
 request.add_header("Accept", "application/json")
 probe_list = []
+probe_asn = Counter()
 try:
     conn = urllib2.urlopen(request)
     result = json.load(conn) 
@@ -24,6 +27,7 @@ try:
             if obj['status'] == 1 and None != obj.get('address_v4', None):
                 print "{0:d} {1:<20s} {2}".format(obj['id'], obj['address_v4'], obj['asn_v4'])
                 probe_list.append(dict((k, obj[k]) for k in ('id', 'address_v4', 'asn_v4')))
+                probe_asn[obj['asn_v4']] += 1
         if next_url is not None:
             conn = urllib2.urlopen(base_url + next_url)
             result = json.load(conn) 
@@ -36,3 +40,14 @@ conn.close()
 print len(probe_list)
 with open('data/probes.json', 'wb') as probe_file:
     json.dump(probe_list, probe_file)
+
+probes_per_origin = pd.DataFrame()
+with open('data/as-list.txt', 'wb') as asn_file:
+    for asn, count in probe_asn.iteritems():
+        asn_file.write("%s\n" % asn)
+        probes_per_origin.append(dict(origin=asn, count=count), ignore_index=True)
+
+origin_weight = pd.read_csv('data/relevant-origin.csv', sep="\t", header=None, names=('origin', 'weight'))
+
+print probes_per_origin
+print origin_weight
