588fa0293875a231c647cedc1c9a05d63e3ec7ef
Sebastian Castro <sebastian@nzrs.net.nz>
2015-03-19 17:23:48 +1300

    Bug fix, the strings given to ipaddress need to be in unicode. Added progress bar. Speed improvements by reducing the number of prefixes to compare against, and adding all addresses to a list first and then check if they are in the prefix list for NZ. Still really slow with large datasets.

--- find-targets-from-scans.py
+++ find-targets-from-scans.py
@@ -4,23 +4,25 @@ import gzip
 import csv
 import json
 import ipaddress
+from progressbar import ProgressBar
 
-nz_addr = set()
-other_addr = set()
 nz_prefix = set()
-nz_prefix_list = []
+failed_addr = set()
 
 
 def is_ip_contained(a):
     contained = False
     try:
-        ip = ipaddress.ip_network(a)
-        for p in nz_prefix_list:
-            if p.overlaps(ip):
-                contained = True
-                break
+        ip = ipaddress.ip_network(unicode(a))
     except ValueError:
-        print "ERROR: address caused exception", a
+        # print "ERROR: address caused exception '%s'" % a
+        failed_addr.add(a)
+        return False
+
+    for p in nz_prefix_list:
+        if p.overlaps(ip):
+            contained = True
+            break
 
     return contained
 
@@ -31,10 +33,10 @@ with open('data/rv-nz-aspath.json', 'rb') as nz_prefixes:
             nz_prefix.add(prefix)
 
 """We need some objects to check if an IP address is covered by a prefix, better to do it once"""
-for prefix in nz_prefix:
-    nz_prefix_list.append(ipaddress.ip_network(prefix))
+nz_prefix_list = [p2 for p2 in ipaddress.collapse_addresses([ipaddress.ip_network(p1) for p1 in nz_prefix])]
 
 
+addr_list = set()
 with gzip.open('data/scans/20150309-dns-53.csv.gz', 'rb') as dns_scan:
     csv_r = csv.reader(dns_scan)
 
@@ -45,17 +47,33 @@ with gzip.open('data/scans/20150309-dns-53.csv.gz', 'rb') as dns_scan:
     for row in csv_r:
         # row[1] is the address we want to check
         idx += 1
-        if not row[1] in nz_addr and not row[1] in other_addr:
-            """ I haven't seen this address before, check against the list of prefixes of interest"""
-            if is_ip_contained(row[1]):
-                nz_addr.add(row[1])
-            else:
-                other_addr.add(row[1])
+        addr_list.add(row[1])
 
-        if idx % 10000 == 0:
-            print "."
+        if idx % 100000 == 0:
+            print "Read %d records" % idx
 
-with open('data/nz-dest-addr.csv', 'wb') as nz_addr_file:
-    csv_w = csv.writer(nz_addr_file)
-    csv_w.writerows(nz_addr)
+        if idx > 100000:
+            break
 
+print "%d addresses will be checked" % (len(addr_list))
+print "%d NZ prefixes" % (len(nz_prefix_list))
+nz_addr = set()
+pbar = ProgressBar(maxval=len(addr_list)).start()
+cnt = 0
+for addr in addr_list:
+    if is_ip_contained(addr):
+        nz_addr.add(addr)
+    cnt += 1
+    if cnt % 100 == 0:
+        pbar.update(cnt)
+
+pbar.finish()
+
+
+print "%d NZ addresses found" % len(nz_addr)
+with open('data/nz-dest-addr.json', 'wb') as nz_addr_file:
+    json.dump([a for a in nz_addr], nz_addr_file)
+
+print "%d failed addresses" % len(failed_addr)
+with open('data/failed-addresses.json', 'wb') as failed_file:
+    json.dump([a for a in failed_addr], failed_file)
