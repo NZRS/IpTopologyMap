cd65a673d4074cdaadba5ce2745f5619708c0199
Sebastian Castro <sebastian@nzrs.net.nz>
2015-04-17 16:24:17 +1200

    Selects random addresses for tracerouting from a data file. To go around a restriction in the number of simultaneous measurements running, created a staged execution, and the ability to recycle a data directory by reading the existing status and appending to it. Stage 3 now schedules traceroutes to known sites in NZ

--- probe-to-probe-traceroute.py
+++ probe-to-probe-traceroute.py
@@ -5,8 +5,38 @@ import argparse
 import urllib2
 import random
 import time
-import sys, os
+import sys
+import os
+import os.path
 from collections import defaultdict
+
+
+def load_existing(source_dir, filename):
+    e = []
+    file_loc = "{}/{}".format(source_dir, filename)
+    if os.path.isfile(file_loc):
+        with open(file_loc, 'rb') as s_file:
+            e = json.load(s_file)
+
+    return e
+
+
+def get_address_from_sample(infile):
+    with open(infile, 'rb') as nz_addr_file:
+        nz_dest = json.load(nz_addr_file)
+
+    """Group the addresses by the prefix covering it"""
+    dest_addr = defaultdict(list)
+    for dest in nz_dest:
+        dest_addr[dest['prefix']].append(dest['address'])
+
+    sample = []
+    """Pick one address per prefix"""
+    for prefix, addresses in dest_addr.iteritems():
+        for a in random.sample(addresses, 1):
+            sample.append(a)
+
+    return sample
 
 
 def read_auth(filename):
@@ -17,7 +47,7 @@ def read_auth(filename):
 
 
 def probe_ids(probe_set, myid):
-    return [str(id) for id in probe_set-set([myid])]
+    return [str(id) for id in probe_set-{myid}]
 
 
 def schedule_measurement(dest, probes):
@@ -26,7 +56,7 @@ def schedule_measurement(dest, probes):
     data = {"definitions": [
         {
             "target": dest,
-            "description": "Traceroute to address {}".format(dest),
+            "description": "Traceroute to {}".format(dest),
             "type": "traceroute",
             "protocol": "ICMP",
             "paris": 16,
@@ -50,7 +80,8 @@ def schedule_measurement(dest, probes):
         for m in results['measurements']:
             msm_status['list'].append(m)
         conn.close()
-        time.sleep(1)
+        # This sleep is important to give time to the scheduled measurements to complete before trying more.
+        time.sleep(3)
     except urllib2.HTTPError as e:
         msm_status['failed'].append(dest)
         print "Fatal Error: {}".format(e.read())
@@ -60,7 +91,13 @@ def schedule_measurement(dest, probes):
 
 parser = argparse.ArgumentParser("Creates probe to probe traceroutes")
 parser.add_argument('--datadir', required=True, help="directory to save output")
+parser.add_argument('--stage', required=False, help="Stage of measurement to execute (1,2,3,all)")
 args = parser.parse_args()
+if (args.stage is None) or (args.stage == 'all'):
+    stage = [1, 2, 3]
+else:
+    stage = [int(s) for s in args.stage.split(",")]
+
 
 if not os.path.exists(args.datadir):
     os.makedirs(args.datadir)
@@ -79,16 +116,34 @@ msm_list = []
 probe_id_set = set([probe['id'] for probe in probe_list])
 failed_msm = []
 """First stage: Schedule the measurements going to an specific probe from all the remaining available probes"""
-for probe in probe_list:
-    status = schedule_measurement(probe['address_v4'], probe_ids(probe_id_set, probe['id']))
-    msm_list = msm_list + status['list']
-    failed_msm = failed_msm + status['failed']
+if 1 in stage:
+    print "Executing Stage 1"
+    for probe in probe_list:
+        status = schedule_measurement(probe['address_v4'], probe_ids(probe_id_set, probe['id']))
+        msm_list = msm_list + status['list']
+        failed_msm = failed_msm + status['failed']
 
 """Second stage: Schedule the measurement to a selected address from a sample"""
+if 2 in stage:
+    print "Executing Stage 2"
+    for addr in random.sample(get_address_from_sample('data/nz-dest-addr.json'), 60):
+        status = schedule_measurement(addr, [str(id) for id in probe_id_set])
+        msm_list = msm_list + status['list']
+        failed_msm = failed_msm + status['failed']
 
+"""Third stage: Sent traceroute to known sites"""
+if 3 in stage:
+    print "Executing Stage 3"
+    with open('data/known-sites.txt', 'rb') as site_file:
+        for site in site_file:
+            status = schedule_measurement(site.rstrip(), [str(id) for id in probe_id_set])
+            msm_list = msm_list + status['list']
+            failed_msm = failed_msm + status['failed']
 
+existing_msm = load_existing(args.datadir, 'measurements.json')
 with open('{}/measurements.json'.format(args.datadir), 'wb') as msm_file:
-    json.dump(msm_list, msm_file)
+    json.dump(msm_list + existing_msm, msm_file)
 
+existing_failures = load_existing(args.datadir, 'failed-probes.json')
 with open('{}/failed-probes.json'.format(args.datadir), 'wb') as failed_file:
-    json.dump(failed_msm, failed_file)
+    json.dump(failed_msm + existing_failures, failed_file)
