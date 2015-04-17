abdd9192a8221329b7c561bc739e1f53ad6ed816
Sebastian Castro <sebastian@nzrs.net.nz>
2015-04-17 16:25:42 +1200

    Testing process pooling to do some paralel work. Test code

--- pool-test.py
+++ pool-test.py
@@ -1,1 +1,15 @@
 __author__ = 'secastro'
+
+from multiprocessing import Pool
+import os
+
+
+def f(x):
+    print "Doing work by ", os.getpid()
+    return x*x
+
+if __name__ == '__main__':
+    pool = Pool(processes=4)
+    result = pool.apply_async(f, [10])
+    print result.get(timeout=1)
+    print pool.map(f, range(20))
