207a2766c5a10bfd80606601e3f02b674742faa9
Aaron Craig <aaron@nzrs.net.nz>
2015-07-02 12:04:19 +1200

    add a unified geolocation lookup module cross-validating geolocation results

---
+++ geoloc.py
@@ -0,0 +1,224 @@
+__author__ = 'craig'
+
+'''
+This module provides a unified geolocation lookup service across multiple
+databases. Useful for cross-referencing geolocation answers.
+'''
+
+## Imports.
+## ----------------------------------------------------------------------
+
+import GeoIP
+import IP2Location
+from radix import Radix
+from netaddr import IPAddress
+from bisect import bisect
+import json
+
+
+
+
+## Geoloc
+## ----------------------------------------------------------------------
+
+# dict of database name to data structure
+_dbs = {
+    'geoip' : None,
+    'ip2location' : None,
+    'known_networks' : None,
+    'dbip' : None,
+    'ipligence' : None
+}
+
+# tables telling you what is currently available to use
+supported_queries = ['country_code']
+available_dbs = []
+
+def quickload(data_dir):
+    '''
+    Loads all known geolocation services using their default filenames from the given working directory.
+    :param data_dir: directory containing files
+    '''
+    load_db("geoip", data_dir + "/GeoIP.dat")
+    load_db("ip2location", data_dir + "/IP-COUNTRY.bin")
+    load_db("known_networks", data_dir + "/known-networks.json")
+    load_db("dbip", data_dir + "/dbip-country-2015-07.csv")
+    load_db("ipligence", data_dir + "/ipligence-lite.csv")
+
+def load_db(db_name, db_fpath):
+    '''
+    Load a geolocation service.
+    :param db_name: name of the geolocation service.
+    :param db_fpath: location of its data file.
+    '''
+
+    # validate database name
+    if db_name not in _dbs.keys(): raise ValueError("Database must be one of: {}".format(_dbs.keys()))
+
+    # load appropriate database
+    if db_name == "geoip": _dbs[db_name] = GeoIP.open(db_fpath, GeoIP.GEOIP_STANDARD)
+    elif db_name == "ip2location": _dbs[db_name] = IP2Location.IP2Location(db_fpath)
+    elif db_name == "known_networks":
+        _dbs['known_networks'] = Radix()
+        with open(db_fpath, 'rb') as known_net_file:
+            known_networks = json.load(known_net_file)
+        for p in known_networks:
+            # Only keep prefices whose country we know
+            if 'country' in p:
+                n = _dbs['known_networks'].add(p['net'])
+                n.data['cc'] = p['country']
+    elif db_name == "dbip": _dbs[db_name] = DBIP(db_fpath)
+    elif db_name == "ipligence": _dbs[db_name] = IPligence(db_fpath)
+    else:
+        raise ValueError("error loading db {}".format(db_name))
+
+    # update table of supported databases
+    global available_dbs
+    available_dbs = [x for x in _dbs.keys() if x is not None]
+
+def query(to_query, ip_addr, db_name):
+    '''
+    Query a given database for a property of the address.
+    Use for making lots of queries.
+    :param to_query: attribute to query.
+    :param ip_addr: address to lookup.
+    :param db_name: name of database to query.
+    :return: str result of the query.
+    '''
+    if to_query not in supported_queries: raise ValueError("Query not supported for {}".format(to_query))
+    if to_query == "country_code": return country_code(ip_addr, db_name)
+    else: raise ValueError("Could not perform {} query in {} database".format(to_query, db_name))
+
+def query_all(to_query, ip_addr):
+    '''
+    Make the given query for every database.
+    :param to_query: the field to query.
+    :param ip_addr: ip address to lookup.
+    :return: { str : str }, database to query result
+    '''
+    return { db : query(to_query, ip_addr, db) for db in available_dbs }
+
+def country_code(ip_addr, db_name):
+    '''
+    Query the country code of the ip address from the specified database.
+    :param ip_addr: ip address to query
+    :param db_name: geolocation db to query from
+    :return: country as given by the db. Raises ValueError if db has not yet been created.
+    '''
+
+    # validate query
+    if db_name not in _dbs.keys(): raise ValueError("Database must be one of: {}".format(_dbs.keys()))
+    if _dbs[db_name] is None: raise ValueError("{} has not been created yet (use geoloc.load_db)".format(db_name))
+
+    # perform query
+    if db_name == 'geoip': return _dbs[db_name].country_code_by_addr(ip_addr)
+    elif db_name == 'ip2location': return _dbs[db_name].get_country_short(ip_addr)
+    elif db_name == 'known_networks': return _dbs[db_name].search_best(network=ip_addr, masklen=32)
+    elif db_name == 'dbip': return _dbs[db_name].country_code(ip_addr)
+    elif db_name == 'ipligence': return _dbs[db_name].country_code(ip_addr)
+    else: raise ValueError("error querying {} db".format(db_name))
+
+def country_code_all(ip_addr):
+    '''
+    Query all databases for the country code of the ip address.
+    :param ip_addr: ip address to look up
+    :return: { str : str }, a mapping of database -> result
+    '''
+    return { db : country_code(ip_addr, db) for db in available_dbs }
+
+
+## DBIP.
+## ----------------------------------------------------------------------
+'''
+A wrapper for querying data in the .csv file provided by dbip.
+'''
+
+
+class DBIP(object):
+    '''
+    This is basically a dict of ip -> country code.
+    '''
+
+    def __init__(me, fpath):
+        with open(fpath, 'rb') as f:
+            me.values = {}
+            for line in f:
+                # line looks like: "IP_ADDR", ___, "COUNTRY_CODE"
+                line = line.split(',')
+                # every value has quotes around it so strip them
+                # also strip end line characters
+                me.values[line[0][1:-1]] = line[2].rstrip()[1:-1]
+
+    def country_code(me, ip_addr):
+        '''
+        Query given country code.
+        :param ip_addr: address to query.
+        :return: str
+        '''
+        return me.values[ip_addr] if ip_addr in me.values else None
+
+
+
+## IPligence.
+## ----------------------------------------------------------------------
+class IPligence(object):
+    '''
+    A wrapper for the IPligence .csv, which stores country by blocks of
+    IP addresses. The ranges are non-overlapping, so we store as a
+    monotonically increasing list of elements (store the range by its
+    starting point). We keep a mapping of these intervals to their
+    country code. To retrieve the CC from a number, we get the index where
+    it _would_ be added ala binary search and lookup the mapping for the
+    interval in that index.
+
+    e.g. for the intervals [0-9], [10-19], [20-29], our list is:
+        [0, 10, 20, 30]
+    if we look up 16, binary search will return the index of 20.
+    So we go to the left one index, get 10, and look up what 10 maps to.
+    The last index is the 'end range'. If something is greater than the
+    last element or less than the first element it is out of the range
+    represented by this list.
+    '''
+
+    def __init__(me, fpath):
+        me.intervals = []
+        me.mapping = {}
+
+        with open(fpath, 'rb') as f:
+            for line in f:
+
+                # stored in .csv file
+                line = line.split(',')
+
+                # strip quote marks
+                start, end = int(line[0][1:-1]), int(line[1][1:-1])
+                cc = line[2][1:-1]
+
+                # no recorded cc for this range
+                if len(cc) == 0: continue
+
+                # the data file lists the intervals in ascending order
+                # so we can append them straight onto the end and it will be sorted.
+                me.intervals.append(start)
+                me.mapping[start] = cc
+
+            # store the ending interval for last range
+            me.mapping[end] = None
+
+    def country_code(me, ip_addr):
+        '''
+        Look up country code of an address.
+        :param ip_addr: ip address to look up.
+        '''
+
+        # convert dotted notation to the int representation
+        ip_intfmt = int(IPAddress(ip_addr))
+
+        # out of range of valid IPs
+        if ip_intfmt < me.intervals[0] or ip_intfmt > me.intervals[-1]:
+            return None
+
+        # use binary search to look up appropriate range
+        indx = bisect(me.intervals, ip_intfmt) - 1
+        val = me.intervals[indx]
+        return me.mapping[val] if val in me.mapping else None
